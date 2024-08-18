0b4bbe40327820cb9ffe090b9977cdfa5431cc4d
pc <shariarfahim21@gmail.com>
2024-08-18 20:56:51 +0600

    new change

--- app/src/main/java/com/fahimshariar/pagecurlnub/profile.java
+++ app/src/main/java/com/fahimshariar/pagecurlnub/profile.java
@@ -1,9 +1,12 @@
 package com.fahimshariar.pagecurlnub;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,27 +17,32 @@ import androidx.core.view.WindowInsetsCompat;
 public class profile extends AppCompatActivity {
 
     TextView profile_email;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });*/
+        });
 
         profile_email = findViewById(R.id.profile_email);
 
-        profile_email.setOnClickListener(new View.OnClickListener() {
+      /*  profile_email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent myintent = new Intent(profile.this, profile.class);
-                startActivity(myintent);
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto", "abc@gmail.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "This is my subject text");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "This is the body of the email");
+                context.startActivity(Intent.createChooser(emailIntent, "Choose an email client"));
             }
-        });
+        });*/
+
 
 
     }
